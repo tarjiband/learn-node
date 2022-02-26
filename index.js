8c9e1d33f4fa055cfb900516be553f6c01e8ac13
Amir <tarjiband@gmail.com>
2022-02-26 05:26:09 +0330

    nodemon installed

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// import express from "express";
-const { urlencoded } = require("body-parser");
-const bodyParser = require("body-parser");
-const express = require("express");
+const name1= "Amir";
+const name2 = 'Ali';
+
+function sayHi(name) {
+    console.log(name);
+}
 
 
-const app = express();
-app.use("/", bodyParser.urlencoded({extended:true}));
-app.get("/", (req, res)=>{
-    // res.write("Hey");
-    
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/", (req, res)=>{
-    let first = Number(req.body.num1);
-    let second = Number(req.body.num2);
-    res.send(`${first+second}`);
-})
-
-// app.get("/about-me", (req,res)=> {
-//     res.sendFile(__dirname + "/about-me.html");
-// })
-
-app.listen(3000, ()=>{
-    console.log("My name is Amir and we are listening on port 3000");
-})
+sayHi(name1);
+sayHi(name2);
